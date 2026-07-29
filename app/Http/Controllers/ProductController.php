9686084c4fd1542c682b736f5de0496dbcda8cf5
RusdiEneri <nuruddinrusydiilham@gmail.com>
2026-07-29 23:49:49 +0700

    fix: import URL facade & perbaiki PSR-4 ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Seller;
+// namespace App\Http\Controllers\Seller;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
